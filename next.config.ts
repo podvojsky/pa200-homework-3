2837ba3c43696a9e2305a804c9e03fd0be4f3754
Lukáš Podvojský <lucas.podvojsky@gmail.com>
2025-05-31 19:29:10 +0200

    feat: lets go

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,5 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-	reactStrictMode: true,
-	distDir: 'build',
-	output: 'standalone',
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
